d0b8551397fec65465654b2fc8a9620d574fc2c6
= <=>
2024-10-07 16:41:51 +0500

    Added BuildingBlocks for common libraries, realized CQRS for creating product

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create Product entity from command object
+
+            var product = new Product()
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            //save to database
+
+
+
+            //return CreateProductResult result 
+
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+
+}
