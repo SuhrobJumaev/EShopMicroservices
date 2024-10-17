294f74324ab37f92bcfbe0a92156dbdc2fc438e0
= <=>
2024-10-17 15:01:06 +0500

    Developed store basket feature

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -1,6 +1,28 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketEndpoints
+    public record StoreBasketRequest(ShoppingCart Cart);
+    public record StoreBasketResponse(string UserName);
+
+    public class StoreBasketEndpoints : CarterModule
     {
+        public override void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBasketCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<StoreBasketResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("StoreBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Store Basket")
+            .WithDescription("Store Basket");
+        }
     }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,29 @@
-﻿namespace Basket.API.Basket.StoreBasket
+﻿
+namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class StoreBasketCommandHandler
+        : ICommandHandler<StoreBasketCommand,StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+            //TODO Store in DB
+            //TODO Update chash
+
+            return new StoreBasketResult("swn");
+        }
     }
 }
